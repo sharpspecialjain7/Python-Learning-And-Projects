4a24896387fe974146883e75d4c9bb4165880e0b
Abhishek Jain <sharpspecial.jain7@gmail.com>
2022-05-10 19:35:39 +0530

    added printing-In-Python

---
+++ Printing-In-Python/main.py
@@ -0,0 +1,20 @@
+# printing string
+print("Hello Abhishek")
+
+# printing string using single quotes
+print('Hello Abhishek using single quotes')
+
+# priting integers
+print(15)
+
+# printing with both single and double quots
+print("print('print function in python')")
+
+# using escape character
+print("What\'s up")
+print("Hello\tWorld") # Tab
+print("Hello\nAbhishek") # New Line
+
+# String concatenation
+print("Hello" + " "+"Abhishek")
+
