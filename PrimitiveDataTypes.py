3c8b26e2c677cb1d03743f1c2e340de20885af95
Abhishek Jain <sharpspecial.jain7@gmail.com>
2022-05-19 10:51:40 +0530

    primitive data types

---
+++ PrimitiveDataTypes.py
@@ -0,0 +1,21 @@
+"""
+1. String, int, float and Boolean are primitive data types in python
+"""
+
+name = "Hello"
+age = 30
+salary = 1234.56
+isMarried = True
+
+# print type or type check
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(isMarried))
+
+# type error example
+# print(len(age))
+# print("Hello " + name +" your age is " + age)
+
+# type conversion
+print("Hello " + name +" your age is " + str(age))
