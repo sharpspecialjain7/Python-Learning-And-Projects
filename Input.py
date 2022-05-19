1ba40032fcbca3b6d658903ec28aea249e02f2be
Abhishek Jain <sharpspecial.jain7@gmail.com>
2022-05-19 10:35:08 +0530

    Input function in python

---
+++ Input.py
@@ -0,0 +1,10 @@
+# Input function is used to take input from user.
+# Anything entered using input function is converted to string
+# Type conversion needs to be take up by programmer
+
+name = input("What is your name? ")
+print(name)
+
+age = int(input("Enter your age: "))
+print(age)
+print(type(age))
