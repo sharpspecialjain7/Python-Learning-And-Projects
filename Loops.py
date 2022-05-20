121c48cb52f351463eb5978461887174c178e479
Abhishek Jain <sharpspecial.jain7@gmail.com>
2022-05-20 11:28:09 +0530

    loops in python

---
+++ Loops.py
@@ -0,0 +1,18 @@
+list = [1,2,3,4,5,6,7,8,9,10]
+
+# for loop when iterating lists
+for num in list:
+    print(num)
+
+# for loop with range
+
+for i in range(1,5):
+    print(i)    
+
+# while loop
+
+num = 5
+while num > 0:
+    print(num)
+    num -=1
+    
